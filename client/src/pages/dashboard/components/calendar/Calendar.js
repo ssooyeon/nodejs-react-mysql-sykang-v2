fa09fc15842fcbd22c126df668f4312fea25ef45
sykang <sykang@inspace.re.kr>
2022-02-11 17:59:28 +0900

    dashboard calendar component to hooks

--- client/src/pages/dashboard/components/calendar/Calendar.js
+++ client/src/pages/dashboard/components/calendar/Calendar.js
@@ -1,63 +1,59 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import DayNames from "./DayNames";
 import uuid from "uuid/v4";
 import Week from "./Week";
 import moment from "moment/moment";
 import s from "./Calendar.module.scss";
 
-class Calendar extends Component {
-  state = {
-    selectedMonth: moment(),
-    selectedDay: moment().startOf("day"),
-    selectedMonthEvents: [
-      {
-        title: "The flower bed",
-        info: "Contents here",
-        itemStyle: "#1870dc",
-        date: moment(`${moment().year()}-${moment().month() + 1}-02`, "YYYY-MM-DD"),
-      },
-      {
-        title: "Stop world water pollution",
-        info: "Have a kick off meeting with .inc company",
-        itemStyle: "#f0b518",
-        date: moment(`${moment().year()}-${moment().month() + 1}-05`, "YYYY-MM-DD"),
-      },
-      {
-        title: "Light Blue Template 1.0.0 release",
-        info: "Some contents here",
-        itemStyle: "#2d8515",
-        date: moment(`${moment().year()}-${moment().month() + 1}-18`, "YYYY-MM-DD"),
-      },
-      {
-        title: "A link",
-        info: "",
-        itemStyle: "#f45722",
-        link: "http://www.flatlogic.com",
-        date: moment(`${moment().year()}-${moment().month() + 1}-29`, "YYYY-MM-DD"),
-      },
-    ],
-    showEvents: false,
+const initialMonthEvents = [
+  {
+    title: "The flower bed",
+    info: "Contents here",
+    itemStyle: "#1870dc",
+    date: moment(`${moment().year()}-${moment().month() + 1}-02`, "YYYY-MM-DD"),
+  },
+  {
+    title: "Stop world water pollution",
+    info: "Have a kick off meeting with .inc company",
+    itemStyle: "#f0b518",
+    date: moment(`${moment().year()}-${moment().month() + 1}-05`, "YYYY-MM-DD"),
+  },
+  {
+    title: "Light Blue Template 1.0.0 release",
+    info: "Some contents here",
+    itemStyle: "#2d8515",
+    date: moment(`${moment().year()}-${moment().month() + 1}-18`, "YYYY-MM-DD"),
+  },
+  {
+    title: "A link",
+    info: "",
+    itemStyle: "#f45722",
+    link: "http://www.flatlogic.com",
+    date: moment(`${moment().year()}-${moment().month() + 1}-29`, "YYYY-MM-DD"),
+  },
+];
+
+export default function Calendar() {
+  const [selectedMonth, setSelectedMonth] = useState(moment());
+  const [selectedDay, setSelectedDay] = useState(moment().startOf("day"));
+  const [selectedMonthEvents, setSelectedMonthEvents] = useState(initialMonthEvents);
+  const [showEvents, setShowEvents] = useState(false);
+
+  const previous = () => {
+    setSelectedMonth(selectedMonth.subtract(1, "month"));
   };
 
-  previous = () => {
-    this.setState({
-      selectedMonth: this.state.selectedMonth.subtract(1, "month"),
-    });
+  const next = () => {
+    setSelectedMonth(selectedMonth.add(1, "month"));
   };
 
-  next = () => {
-    this.setState({
-      selectedMonth: this.state.selectedMonth.add(1, "month"),
-    });
+  const renderMonthLabel = () => {
+    return <span className={`${s.calendarItemContainer} ${s.monthLabel}`}>{selectedMonth.format("MMMM YYYY")}</span>;
   };
 
-  renderMonthLabel = () => {
-    return <span className={`${s.calendarItemContainer} ${s.monthLabel}`}>{this.state.selectedMonth.format("MMMM YYYY")}</span>;
-  };
-
-  renderWeeks = () => {
-    const currentMonthView = this.state.selectedMonth;
-    const currentSelectedDay = this.state.selectedDay;
+  const renderWeeks = () => {
+    const currentMonthView = selectedMonth;
+    const currentSelectedDay = selectedDay;
 
     let weeks = [];
     let done = false;
@@ -69,7 +65,7 @@ class Calendar extends Component {
       weeks.push(
         <Week
           key={uuid()}
-          selectedMonthEvents={this.state.selectedMonthEvents}
+          selectedMonthEvents={selectedMonthEvents}
           previousCurrentNextView={previousCurrentNextView.clone()}
           currentMonthView={currentMonthView}
           selected={currentSelectedDay}
@@ -82,25 +78,21 @@ class Calendar extends Component {
     return weeks;
   };
 
-  render() {
-    return (
-      <div className={`${s.calendarRectangle}`}>
-        <div>
-          <section className={`${s.mainCalendar}`}>
-            <header className={`${s.calendarHeader}`}>
-              <div className={`${s.calendarRow} ${s.titleHeader}`}>
-                <i className={`${s.calendarItemContainer} ${s.arrow} la la-arrow-left`} onClick={this.previous} />
-                <div className={`${s.calendarItemContainer} ${s.headerText}`}>{this.renderMonthLabel()}</div>
-                <i className={`${s.calendarItemContainer} ${s.arrow} la la-arrow-right`} onClick={this.next} />
-              </div>
-              <DayNames />
-            </header>
-            <div className={`${s.daysContainer}`}>{this.renderWeeks()}</div>
-          </section>
-        </div>
+  return (
+    <div className={`${s.calendarRectangle}`}>
+      <div>
+        <section className={`${s.mainCalendar}`}>
+          <header className={`${s.calendarHeader}`}>
+            <div className={`${s.calendarRow} ${s.titleHeader}`}>
+              <i className={`${s.calendarItemContainer} ${s.arrow} la la-arrow-left`} onClick={previous} />
+              <div className={`${s.calendarItemContainer} ${s.headerText}`}>{renderMonthLabel()}</div>
+              <i className={`${s.calendarItemContainer} ${s.arrow} la la-arrow-right`} onClick={next} />
+            </div>
+            <DayNames />
+          </header>
+          <div className={`${s.daysContainer}`}>{renderWeeks()}</div>
+        </section>
       </div>
-    );
-  }
+    </div>
+  );
 }
-
-export default Calendar;
--- client/src/pages/dashboard/components/calendar/Day.js
+++ client/src/pages/dashboard/components/calendar/Day.js
@@ -1,84 +1,67 @@
-import React, { Component } from 'react'
-import { Popover, PopoverHeader, PopoverBody, Tooltip,} from 'reactstrap';
-import s from './Calendar.module.scss'
+import React, { useEffect, useState } from "react";
+import { Popover, PopoverHeader, PopoverBody, Tooltip } from "reactstrap";
+import s from "./Calendar.module.scss";
 
-class Day extends Component {
-  state = {
-    popoverShow: false,
-    tooltipShow: false
-  }
+export default function Day(props) {
+  const { day, selected } = props;
 
-  togglePopover = () => {
-    this.setState({ popoverShow: !this.state.popoverShow })
-  }
+  const [popoverShow, setPopoverShow] = useState(false);
+  const [tooltipShow, setTooltipShow] = useState(false);
 
-  toggleTooltip = () => {
-    this.setState({ tooltipShow: !this.state.tooltipShow })
-  }
+  const togglePopover = () => {
+    setPopoverShow(!popoverShow);
+  };
 
-  render() {
-    const { day, selected } = this.props;
-    return (
-      <div className={
-          `${s.day}` +
-          (day.isToday ? ` ${s.today}` : "") +
-          (day.isCurrentMonth ? "" : ` ${s.differentMonth}`) +
-          (day.date.isSame(selected) ? ` ${s.selected}` : "") +
-          (day.hasEvents ? ` ${s.hasEvents}` : "") } > 
+  const toggleTooltip = () => {
+    setTooltipShow(!tooltipShow);
+  };
 
-        {!day.hasEvents ? 
-        <div className={s.dayNumber}>{day.number}</div> 
-        : (day.hasEvents && day.link) 
-        ? 
+  return (
+    <div
+      className={
+        `${s.day}` +
+        (day.isToday ? ` ${s.today}` : "") +
+        (day.isCurrentMonth ? "" : ` ${s.differentMonth}`) +
+        (day.date.isSame(selected) ? ` ${s.selected}` : "") +
+        (day.hasEvents ? ` ${s.hasEvents}` : "")
+      }
+    >
+      {!day.hasEvents ? (
+        <div className={s.dayNumber}>{day.number}</div>
+      ) : day.hasEvents && day.link ? (
         <React.Fragment>
           <a
             rel="noopener noreferrer"
             target="_blank"
-            onMouseEnter={this.toggleTooltip}
-            onMouseOut={this.toggleTooltip}
+            onMouseEnter={toggleTooltip}
+            onMouseOut={toggleTooltip}
             id={`Tooltip${day.number}`}
-            href={day.link ? day.link : "#"} 
-            className={s.dayNumber}> {day.number}
-            {day.itemStyle ? 
-              <span 
-                style={{backgroundColor: `${day.itemStyle}`}} 
-                className={s.calendarDot}>
-              </span> : "" }
+            href={day.link ? day.link : "#"}
+            className={s.dayNumber}
+          >
+            {" "}
+            {day.number}
+            {day.itemStyle ? <span style={{ backgroundColor: `${day.itemStyle}` }} className={s.calendarDot}></span> : ""}
           </a>
-          <Tooltip 
-            placement="top" 
-            isOpen={this.state.tooltipShow} 
-            toggle={this.toggleTooltip} 
-            target={`Tooltip${day.number}`}>
+          <Tooltip placement="top" isOpen={tooltipShow} toggle={toggleTooltip} target={`Tooltip${day.number}`}>
             {day.title}
           </Tooltip>
-        </React.Fragment> 
-        : (day.hasEvents && !day.link)
-        ? 
-          <React.Fragment>
-            <div
-              onClick={this.togglePopover}
-              id={`Popover${day.number}`}
-              className={s.dayNumber}> {day.number}
-                {day.itemStyle ? 
-                  <span 
-                    style={{backgroundColor: `${day.itemStyle}`}} 
-                    className={s.calendarDot}>
-                  </span> : "" }
-            </div>
-            <Popover 
-              placement="top" 
-              isOpen={this.state.popoverShow} 
-              target={`Popover${day.number}`} 
-              toggle={this.togglePopover}>
-              <PopoverHeader>{day.title}</PopoverHeader>
-              <PopoverBody>{day.info}</PopoverBody>
-            </Popover> 
-          </React.Fragment> 
-        : "" }
-      
-      </div>
-    );
-  }
+        </React.Fragment>
+      ) : day.hasEvents && !day.link ? (
+        <React.Fragment>
+          <div onClick={togglePopover} id={`Popover${day.number}`} className={s.dayNumber}>
+            {" "}
+            {day.number}
+            {day.itemStyle ? <span style={{ backgroundColor: `${day.itemStyle}` }} className={s.calendarDot}></span> : ""}
+          </div>
+          <Popover placement="top" isOpen={popoverShow} target={`Popover${day.number}`} toggle={togglePopover}>
+            <PopoverHeader>{day.title}</PopoverHeader>
+            <PopoverBody>{day.info}</PopoverBody>
+          </Popover>
+        </React.Fragment>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
-export default Day;
