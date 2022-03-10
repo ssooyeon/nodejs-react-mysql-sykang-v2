fc416447aa8fdb2f3ae5da03ce964af8d1be3ce7
sykang <sykang@inspace.re.kr>
2022-03-10 16:02:20 +0900

    remove header search input

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -16,13 +16,10 @@ import {
   DropdownMenu,
   DropdownItem,
   Badge,
-  Form,
-  FormGroup,
 } from "reactstrap";
 import PowerIcon from "../Icons/HeaderIcons/PowerIcon";
 import BellIcon from "../Icons/HeaderIcons/BellIcon";
 import BurgerIcon from "../Icons/HeaderIcons/BurgerIcon";
-import SearchIcon from "../Icons/HeaderIcons/SearchIcon";
 import ArrowIcon from "../Icons/HeaderIcons/ArrowIcon";
 
 import { openSidebar, closeSidebar } from "../../actions/navigation";
@@ -99,18 +96,6 @@ function Header(props) {
             />
           </InputGroup>
         </Collapse>
-        <Form className="d-md-down-none mr-3 ml-3" inline>
-          <FormGroup>
-            <InputGroup className={`input-group-no-border ${s.searchForm}`}>
-              <InputGroupAddon addonType="prepend">
-                <InputGroupText className={s.inputGroupText}>
-                  <SearchIcon className={s.headerIcon} />
-                </InputGroupText>
-              </InputGroupAddon>
-              <Input id="search-input" className="input-transparent" placeholder="Search Dashboard" />
-            </InputGroup>
-          </FormGroup>
-        </Form>
 
         <Nav className="ml-md-0">
           <Dropdown nav isOpen={notificationsOpen} toggle={toggleNotifications} id="basic-nav-dropdown" className={`${s.notificationsMenu}`}>
@@ -133,36 +118,38 @@ function Header(props) {
             </DropdownToggle>
           </Dropdown>
           <NavItem className={`${s.divider} d-none d-sm-block`} />
-          <Dropdown className="d-none d-sm-block" nav isOpen={supportOpen} toggle={toggleSupportDropdown}>
-            <DropdownToggle nav className={`${s.navItem} text-white`}>
-              <BellIcon className={s.headerIcon} />
-              <div className={s.count}></div>
-            </DropdownToggle>
-            <DropdownMenu right className={`${s.dropdownMenu} ${s.support}`}>
-              {notificationList &&
-                notificationList.map((noti, index) => {
-                  return (
-                    <DropdownItem key={noti.id}>
-                      <Badge color={labels[Math.floor(Math.random() * 4)]}>
-                        <i className={icons[Math.floor(Math.random() * 5)]} />
-                      </Badge>
-                      {noti.isDone ? (
-                        <div className={s.details} style={{ textDecoration: "line-through", color: "rgba(244, 244, 245, 0.6)" }}>
-                          {noti.title.length > 30 ? noti.title.substr(0, 30) + "..." : noti.title}
-                        </div>
-                      ) : (
-                        <div className={s.details}>{noti.title.length > 30 ? noti.title.substr(0, 30) + "..." : noti.title}</div>
-                      )}
-                    </DropdownItem>
-                  );
-                })}
-              <DropdownItem>
-                <a href="/#/app/task" className="text-white">
-                  More <ArrowIcon className={s.headerIcon} maskName="bellArrow" />
-                </a>
-              </DropdownItem>
-            </DropdownMenu>
-          </Dropdown>
+          {currentUser ? (
+            <Dropdown className="d-none d-sm-block" nav isOpen={supportOpen} toggle={toggleSupportDropdown}>
+              <DropdownToggle nav className={`${s.navItem} text-white`}>
+                <BellIcon className={s.headerIcon} />
+                <div className={s.count}></div>
+              </DropdownToggle>
+              <DropdownMenu right className={`${s.dropdownMenu} ${s.support}`}>
+                {notificationList &&
+                  notificationList.map((noti, index) => {
+                    return (
+                      <DropdownItem key={noti.id}>
+                        <Badge color={labels[Math.floor(Math.random() * 4)]}>
+                          <i className={icons[Math.floor(Math.random() * 5)]} />
+                        </Badge>
+                        {noti.isDone ? (
+                          <div className={s.details} style={{ textDecoration: "line-through", color: "rgba(244, 244, 245, 0.6)" }}>
+                            {noti.title.length > 30 ? noti.title.substr(0, 30) + "..." : noti.title}
+                          </div>
+                        ) : (
+                          <div className={s.details}>{noti.title.length > 30 ? noti.title.substr(0, 30) + "..." : noti.title}</div>
+                        )}
+                      </DropdownItem>
+                    );
+                  })}
+                <DropdownItem>
+                  <a href="/#/app/task" className="text-white">
+                    More <ArrowIcon className={s.headerIcon} maskName="bellArrow" />
+                  </a>
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          ) : null}
           <NavItem>
             {currentUser ? (
               <NavLink className={`${s.navItem} text-white`} onClick={doLogout}>
