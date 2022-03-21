e788f2eb6d0ae50e32b6e08e6583d0f26ba13edd
sykang <sykang@inspace.re.kr>
2022-03-21 14:18:37 +0900

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 import { Switch, Route, Redirect } from "react-router";
 import { HashRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
@@ -19,30 +19,51 @@ const PrivateRoute = ({ dispatch, component, ...rest }) => {
 
 const CloseButton = ({ closeToast }) => <i onClick={closeToast} className="la la-close notifications-close" />;
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <ToastContainer autoClose={5000} hideProgressBar closeButton={<CloseButton />} />
-        <HashRouter>
-          <Switch>
-            <Route path="/" exact render={() => <Redirect to="/app/main" />} />
-            <Route path="/app" exact render={() => <Redirect to="/app/main" />} />
-            <PrivateRoute path="/app" dispatch={this.props.dispatch} component={LayoutComponent} />
-            <Route path="/register" exact component={Register} />
-            <Route path="/login" exact component={Login} />
-            <Route path="/error" exact component={ErrorPage} />
-            <Route component={ErrorPage} />
-            <Redirect from="*" to="/app/main/dashboard" />
-          </Switch>
-        </HashRouter>
-      </div>
-    );
-  }
+function App(props) {
+  return (
+    <div>
+      <ToastContainer autoClose={5000} hideProgressBar closeButton={<CloseButton />} />
+      <HashRouter>
+        <Switch>
+          <Route path="/" exact render={() => <Redirect to="/app/main" />} />
+          <Route path="/app" exact render={() => <Redirect to="/app/main" />} />
+          <PrivateRoute path="/app" dispatch={props.dispatch} component={LayoutComponent} />
+          <Route path="/register" exact component={Register} />
+          <Route path="/login" exact component={Login} />
+          <Route path="/error" exact component={ErrorPage} />
+          <Route component={ErrorPage} />
+          <Redirect from="*" to="/app/main/dashboard" />
+        </Switch>
+      </HashRouter>
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  isAuthenticated: state.auth.isAuthenticated,
-});
+export default App;
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <ToastContainer autoClose={5000} hideProgressBar closeButton={<CloseButton />} />
+//         <HashRouter>
+//           <Switch>
+//             <Route path="/" exact render={() => <Redirect to="/app/main" />} />
+//             <Route path="/app" exact render={() => <Redirect to="/app/main" />} />
+//             <PrivateRoute path="/app" dispatch={this.props.dispatch} component={LayoutComponent} />
+//             <Route path="/register" exact component={Register} />
+//             <Route path="/login" exact component={Login} />
+//             <Route path="/error" exact component={ErrorPage} />
+//             <Route component={ErrorPage} />
+//             <Redirect from="*" to="/app/main/dashboard" />
+//           </Switch>
+//         </HashRouter>
+//       </div>
+//     );
+//   }
+// }
 
-export default connect(mapStateToProps)(App);
+// const mapStateToProps = (state) => ({
+//   isAuthenticated: state.auth.isAuthenticated,
+// });
+
+// export default connect(mapStateToProps)(App);
