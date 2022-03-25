f559ee396fe9e818f2dd06e56666141a6fd4f0f2
sykang <sykang@inspace.re.kr>
2022-03-25 11:21:05 +0900

    test

--- client/scripts/start.js
+++ client/scripts/start.js
@@ -1,7 +1,7 @@
 "use strict";
 
-process.env.BABEL_ENV = "development";
-process.env.NODE_ENV = "development";
+process.env.BABEL_ENV = "production";
+process.env.NODE_ENV = "production";
 
 process.on("unhandledRejection", (err) => {
   throw err;
