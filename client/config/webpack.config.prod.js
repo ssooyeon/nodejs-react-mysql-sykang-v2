d51bcd307877b06cdf0b83245b48859fb778427b
sykang <sykang@inspace.re.kr>
2022-03-25 11:33:19 +0900

    test3

--- client/config/webpack.config.prod.js
+++ client/config/webpack.config.prod.js
@@ -120,15 +120,19 @@ module.exports = {
   },
   optimization: {
     minimizer: [
-      new UglifyJsPlugin(
-        new UglifyJsPlugin({
-          uglifyOptions: {
-            output: {
-              comments: false,
-            },
-          },
-        })
-      ),
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          warnings: false,
+          parse: {},
+          compress: {},
+          mangle: true, // Note `mangle.properties` is `false` by default.
+          output: null,
+          toplevel: false,
+          nameCache: null,
+          ie8: false,
+          keep_fnames: false,
+        },
+      }),
       new TerserPlugin({
         terserOptions: {
           parse: {
