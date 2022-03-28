57cab1539e121bd6ddfc0445cb170f536279b868
sykang <sykang@inspace.re.kr>
2022-03-28 10:56:38 +0900

    .

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API_HOST,
-  // baseURL: "http://localhost:8081/api",
-  // baseURL: "/api",
   headers: {
     "Content-type": "application/json",
   },
