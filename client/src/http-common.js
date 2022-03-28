9554f3be6f0f39f6518be687d78da351ad02ab56
sykang <sykang@inspace.re.kr>
2022-03-28 11:37:33 +0900

    setting deploy

--- client/src/http-common.js
+++ client/src/http-common.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: process.env.REACT_APP_API_HOST,
+  // baseURL: "http://localhost:8081/api",  // local
+  // baseURL: "/api", // deploy
   headers: {
     "Content-type": "application/json",
   },
