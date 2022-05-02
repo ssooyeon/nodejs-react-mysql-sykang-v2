3e106a8607c87d6164097cc2ba056fb8964bc78a
sykang <sykang@inspace.re.kr>
2022-05-02 09:40:38 +0900

    add schedule color

--- client/src/pages/schedule/AddScheduleModal.js
+++ client/src/pages/schedule/AddScheduleModal.js
@@ -24,6 +24,7 @@ const colorList = [
   "#1fc31f",
   "#0101c3",
   "#c301c3",
+  "#2b3162",
 ];
 const weekList = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const weekAbbr = ["SU", "MO", "TU", "WE", "TH", "FR", "SA"];
